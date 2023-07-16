bd41d6273cfee44e9f7ffd4191998038ba52dceb
Aditya R Deshmukh <126074449+adityadeshmukh011@users.noreply.github.com>
2023-07-16 13:35:15 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+namespace Lab1
+{
+    internal class Program1
+    {
+        static void Main()
+        {
+            string filePath = "C:\\C-DAC STUDY_MATERIAL\\c#.net\\Websites\\Lab1\\mytest.txt";
+            string fileContent = "Hello and Welcome\nIt is the first content\nof the text file mytest.txt in C#";
+
+            // Create the text file and write content to it
+            File.WriteAllText(filePath, fileContent);
+
+            // Read the content from the text file
+            string readContent = File.ReadAllText(filePath);
+
+            // Display the file's content
+            Console.WriteLine($"Here is the content of the file {filePath}:\n{readContent}");
+        }
+    }
+
+
+}
